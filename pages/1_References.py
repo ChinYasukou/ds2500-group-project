d3159b077391e9b5e550b11f57ba00e8934b28d7
ChinYasukou <chinyasukou@gmail.com>
2026-03-24 12:40:29 -0400

    Add files via upload

---
+++ pages/1_References.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.set_page_config(page_title="References", layout="wide")
+
+st.title("References")
+
+st.subheader("Data Source")
+st.write("Behavioral Risk Factor Surveillance System (BRFSS), 2024")
+
+st.subheader("Tools")
+st.write("Python, pandas, matplotlib, Streamlit")
+
+st.subheader("Team")
+st.write("Jinghao Shen, Sharon Wu, Lucy Pesek and Zineb Laghzaoui ")
